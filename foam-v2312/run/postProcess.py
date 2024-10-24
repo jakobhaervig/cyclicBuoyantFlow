970a2247b5f26c86d189612b6d3e6d305cc48cc3
Jakob Hærvig <jakob.haervig@gmail.com>
2024-10-24 10:47:47 +0200

    Corrected Tb and removed a couple of hardcoded values

--- foam-v2312/run/postProcess.py
+++ foam-v2312/run/postProcess.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from pathlib import Path
 
 # Function to load variables from the file with semicolons
 def load_variables(filename):
@@ -25,21 +26,27 @@ def load_variables(filename):
     return variables
 
 # Load the variables from the file
-filename = 'verticalChannel_0000/parms'
+filename = 'verticalChannel_0002/parms'
 vars = load_variables(filename)
 
 #Calc Gr and Pr from vars
-Gr = -1*(vars["g"]*vars["beta"]*(vars["T_set"]-vars["T0"])*vars["b"]**3)/(vars["nu"]**2)
+Gr = np.abs(vars["g"])*vars["beta"]*(vars["T_set"]-vars["T0"])*vars["b"]**3/vars["nu"]**2
 Pr = vars["nu"]/vars["DT"]
 
-psi = np.genfromtxt(r'verticalChannel_0001/psi', delimiter=' ')
-Psi = psi[:,1]*(vars['b']/vars["nu"])
+print(Gr,Pr)
+
+psi_data = np.genfromtxt(r'verticalChannel_0002/psi', delimiter=' ')
+
+time = psi_data[:,0]
+psi = psi_data[:,1]
+
+Psi = psi*(vars['b']/vars["nu"])
 
 plt.xlabel('Time [s]')
 plt.ylabel(r'Dimensionless massflow $\Psi$')
 
-plt.plot(psi[:,0],((Psi-1/12*Gr)/(1/12*Gr))*100, label='non-constant source')
-#plt.hlines((1/12*Gr), 0, psi[-1,0], color='red', label='constant source (1/12Gr) Gr={0}'.format(Gr))
+plt.plot(time,Psi, label='non-constant source')
+plt.hlines((1/12*Gr), 0, time[-1], color='red', label='constant source (1/12Gr) Gr={0}'.format(Gr))
 
 plt.grid('both')
 plt.legend()
